ed5c76b607aa710d7e3cc07a376533fe6b0ad6b8
emingenc <emin@users.noreply.github.com>
2026-08-13 20:16:39 -0700

    deploy: cyberpunk metal tiles (no fake tint) + alignment test update

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786677195|1538182';
+const CACHE_VERSION = '1786677396|1473164';
 /** @type {string} */
 const CACHE_PREFIX = 'HACK://OVERFLOW-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
