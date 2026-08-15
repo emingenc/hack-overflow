96016048d0a07a16ebb87516e1c1081b88718308
emingenc <emin@users.noreply.github.com>
2026-08-14 22:12:38 -0700

    feat: firewall integrity bar (wrong -30%, hint -15%, lock at 0 on L2+), lockout respawn, integrity probe test

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786757611|1769974';
+const CACHE_VERSION = '1786770749|2360173';
 /** @type {string} */
 const CACHE_PREFIX = 'HACK://OVERFLOW-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
