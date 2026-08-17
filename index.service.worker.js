5b1b2400535990ac55742f09294ec9b6306b8724
emingenc <emin@users.noreply.github.com>
2026-08-17 14:14:37 -0700

    polish: level empty-void + ORDER button palette

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786994853|1670129';
+const CACHE_VERSION = '1787001265|1664263';
 /** @type {string} */
 const CACHE_PREFIX = 'HACK://OVERFLOW-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
