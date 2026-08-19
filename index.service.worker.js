99c69f8095b711f19c51173b9bfbc3e6c8e148b7
emingenc <emin@users.noreply.github.com>
2026-08-18 21:14:16 -0700

    polish: player shadow

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787112515|1670330';
+const CACHE_VERSION = '1787112849|1862185';
 /** @type {string} */
 const CACHE_PREFIX = 'HACK://OVERFLOW-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
