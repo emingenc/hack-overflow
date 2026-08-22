a967187bfa4396b867c14199b6c6a08f2e08fca2
emingenc <emin@users.noreply.github.com>
2026-08-21 17:51:18 -0700

    fix: HUD border + player shadow

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787359781|1712359';
+const CACHE_VERSION = '1787359874|1988865';
 /** @type {string} */
 const CACHE_PREFIX = 'HACK://OVERFLOW-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
