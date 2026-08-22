65d5b7eb757af5c9f2dd2bf798f5dce65c0f114b
emingenc <emin@users.noreply.github.com>
2026-08-21 20:02:43 -0700

    perf+polish: lighter bloom, fewer particles, cleaner route map, hidden easter egg

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787364882|1998643';
+const CACHE_VERSION = '1787367759|2611215';
 /** @type {string} */
 const CACHE_PREFIX = 'HACK://OVERFLOW-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
