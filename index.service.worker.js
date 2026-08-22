675165dec7958d620762bfa9c8a0198fd9c9ec1e
emingenc <emin@users.noreply.github.com>
2026-08-21 17:44:53 -0700

    polish: curved route roads

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787359451|1665377';
+const CACHE_VERSION = '1787359490|1493371';
 /** @type {string} */
 const CACHE_PREFIX = 'HACK://OVERFLOW-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
