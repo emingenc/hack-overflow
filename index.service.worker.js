2a54b31004d06b90d1498230a5c6ef23b2fffa20
emingenc <emin@users.noreply.github.com>
2026-08-21 17:49:45 -0700

    polish: HUD terminal material

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787359744|1882841';
+const CACHE_VERSION = '1787359781|1712359';
 /** @type {string} */
 const CACHE_PREFIX = 'HACK://OVERFLOW-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
