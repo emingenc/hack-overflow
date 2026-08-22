cd652545ee3082668ca762e0a4de669eddf180a5
emingenc <emin@users.noreply.github.com>
2026-08-21 20:53:32 -0700

    fix: versioned executable — fresh paths bypass stale cache (memory-access-out-of-bounds)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787369839|1801530';
+const CACHE_VERSION = '1787370577|2132544';
 /** @type {string} */
 const CACHE_PREFIX = 'HACK://OVERFLOW-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
