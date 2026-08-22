75c783da1f6fcd105a58d726371f617604a76aab
emingenc <emin@users.noreply.github.com>
2026-08-21 19:19:27 -0700

    fix: loader shows download progress (first load = 40MB engine)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787360208|1596067';
+const CACHE_VERSION = '1787364882|1998643';
 /** @type {string} */
 const CACHE_PREFIX = 'HACK://OVERFLOW-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
